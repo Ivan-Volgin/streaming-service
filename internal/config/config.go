7bbeb5e250aeaa3d69dd7b919eac517d7369079a
Ivan Volgin <volgin.ivan.05@gmail.com>
2025-03-10 18:11:05 +0300

    feat: added config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,15 @@
 package config
+
+import "time"
+
+type AppConfig struct {
+	LogLevel string
+	Rest     Rest
+}
+
+type Rest struct {
+	ListenAddress string        `envconfig:"PORT"`
+	WriteTimeout  time.Duration `envconfig:"WRITE_TIMEOUT"`
+	ServerName    string        `envconfig:"SERVER_NAME"`
+	Token         string        `envconfig:"TOKEN"`
+}
